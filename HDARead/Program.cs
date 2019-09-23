158fc49da584b9b1dfc0366bc18bc4b0d43823aa
Akorostel <Akorostel@gmail.com>
2019-09-23 19:14:24 +0300

    Parsing command lines attributes moved to separate functions

--- HDARead/Program.cs
+++ HDARead/Program.cs
@@ -29,102 +29,41 @@ using System.IO;
  */
 
 namespace HDARead {
+
+    enum eOutputFormat {
+        LIST = 1,
+        TABLE = 2,
+        MERGED = 3
+    }
+
     class Program {
+        static string Host = null;
+        static string Server = null;
+        static string StartTime = "NOW-1H";
+        static string EndTime = "NOW";
+        static int Aggregate = (int)HDAClient.OPCHDA_AGGREGATE.AVERAGE;
+        static string OutputTimestampFormat = null;
+        static int MaxValues = 10;
+        static int ResampleInterval = 0;
+        static string OutputFileName = null;
+        static string InputFileName = null;
+        static bool ReadRaw = false;
+        static bool Help = false;
+        static eOutputFormat OutputFormat = eOutputFormat.LIST;
+        static List<string> Tagnames = new List<string>();
+        static string OptionDescription = null;
 
         static TraceSource _trace = new TraceSource("ConsoleApplicationTraceSource");
-        enum eOutputFormat {
-            LIST = 1,
-            TABLE = 2,
-            MERGED = 3
-        }
 
         static void Main(string[] args) {
 
-            // Private NumberOfTags As ConInt
-            string Host = null, Server = null;
-            string StartTime = "NOW-1H", EndTime = "NOW";
-            int Aggregate = (int)HDAClient.OPCHDA_AGGREGATE.AVERAGE;
-            string OutputTimestampFormat = null;
-            int MaxValues = 10;
-            int ResampleInterval = 0;
-            string OutputFileName = null;
-            string InputFileName = null;
-            bool read_raw = false;
-            bool show_help = false;
-            eOutputFormat OutputFormat = eOutputFormat.LIST;
-
-            List<string> Tagnames = new List<string>();
-
-            var p = new OptionSet() {
-   	            { "n=|node=",               "Remote computer name (optional)",  v => Host = v },
-   	            { "s=|server=",             "OPC HDA server name (required)",   v => Server = v },
-   	            { "from=|start=|begin=",    "Start time (abs. or relative), default NOW-1H",    
-                                                                                v => StartTime = v ?? "NOW-1H"},
-   	            { "to=|end=",               "End time (abs. or relative), default NOW",      
-                                                                                v => EndTime = v ?? "NOW"},
-   	            { "a=|agg=",                "Aggregate (see spec)",             v => Aggregate = GetHDAAggregate(v) },
-                { "r=|resample=",           "Resample interval (in seconds), 0 - return just one value (see OPC HDA spec.)",  
-                                                                                v => ResampleInterval = Int32.Parse(v)},
-                { "raw",                    "Read raw data (if omitted, read processed data) ",  
-                                                                                v => read_raw = v != null},
-                { "m=|maxvalues=",          "Maximum number of values to load (should be checked at OPC server side, but doesn't work)", 
-                                                                                v => MaxValues = Int32.Parse(v)},
-                { "t=|tsformat=",           "Output timestamp format to use",   v => OutputTimestampFormat = v},
-                { "f=",                     "Output format (LIST or TABLE or MERGED)",   
-                                                                                v => OutputFormat = GetOutputFormat(v)},
-                { "o=|output=",             "Output filename (if omitted, output to console)",   
-                                                                                v => OutputFileName = v},
-                { "i=|input=",              "Input filename with list of tags (if omitted, tag list must be provided as command line argument)",   
-                                                                                v => InputFileName = v},
-                { "h|?|help",               "Show help",                        v => show_help = v != null },
-                { "<>",                     "List of tag names",                v => Tagnames.Add (v)},
-            };
-
-            Console.Write("HDARead: ");
-            try {
-                p.Parse(args);
-            } catch (OptionException e) {
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Try `HDARead --help' for more information.");
+            if (!ParseCommandLine(args)) {
                 return;
             }
-
-            if (show_help) {
-                ShowHelp(p);
+            if (!CheckOptions()) {
                 return;
             }
-
-            if (string.IsNullOrEmpty(Server)) {
-                Console.WriteLine("Missing required option s=|server=");
-                return;
-            }
-
-            if (string.IsNullOrEmpty(InputFileName)) {
-                if (Tagnames.Count() < 1) {
-                    Console.WriteLine("No tagnames were specified.");
-                    return;
-                }
-            } else {
-                if (Tagnames.Count() > 0 ) {
-                    Console.WriteLine("If the input file is specified, no tags may be entered as command line argument");
-                    return;
-                }
-                Tagnames = File.ReadLines(InputFileName).ToList();
-                if (Tagnames.Count() < 1) {
-                    Console.WriteLine("No tagnames were specified.");
-                    return;
-                }
-            }
-
-            Console.WriteLine("HDARead is going to:");
-            Console.WriteLine("\t connect to OPC HDA server named '{0}' on computer '{1}'", Server, Host);
-            Console.WriteLine("\t and read {0} data for the period from '{1}' to '{2}'", read_raw?"raw":"processed", StartTime, EndTime);
-            Console.WriteLine("\t with resample interval {0} seconds", ResampleInterval, EndTime);
-            Console.WriteLine("\t for the following tags:");
-            foreach (string t in Tagnames) {
-                Console.WriteLine("\t\t" + t );
-            }
-            Console.WriteLine("\t No more than {0} values should be loaded (checked only at OPC server side).", MaxValues);
+            ShowInfo();
 
             Opc.Hda.ItemValueCollection[] OPCHDAItemValues = null;
             try {
@@ -133,7 +72,7 @@ namespace HDARead {
                 _trace.TraceEvent(TraceEventType.Verbose, 0, "Created HDAClient");
                 if (srv.Connect(Host, Server)) {
                     _trace.TraceEvent(TraceEventType.Verbose, 0, "Connected. Going to read.");
-                    res = srv.Read(StartTime, EndTime, Tagnames.ToArray(), Aggregate, MaxValues, ResampleInterval, read_raw, out OPCHDAItemValues);
+                    res = srv.Read(StartTime, EndTime, Tagnames.ToArray(), Aggregate, MaxValues, ResampleInterval, ReadRaw, out OPCHDAItemValues);
                 } else {
                     Console.WriteLine("HDARead unable to connect to OPC server.");
                 }
@@ -164,7 +103,7 @@ namespace HDARead {
             }
 
             switch (OutputFormat) {
-                case eOutputFormat.LIST: 
+                case eOutputFormat.LIST:
                     OutputList(writer, OPCHDAItemValues, OutputTimestampFormat);
                     break;
                 case eOutputFormat.MERGED:
@@ -181,6 +120,95 @@ namespace HDARead {
             }
             
             return;
+        }
+
+        static bool ParseCommandLine(string[] args) {
+             var p = new OptionSet() {
+   	            { "n=|node=",               "Remote computer name (optional)",  v => Host = v },
+   	            { "s=|server=",             "OPC HDA server name (required)",   v => Server = v },
+   	            { "from=|start=|begin=",    "Start time (abs. or relative), default NOW-1H",    
+                                                                                v => StartTime = v ?? "NOW-1H"},
+   	            { "to=|end=",               "End time (abs. or relative), default NOW",      
+                                                                                v => EndTime = v ?? "NOW"},
+   	            { "a=|agg=",                "Aggregate (see spec)",             v => Aggregate = GetHDAAggregate(v) },
+                { "r=|resample=",           "Resample interval (in seconds), 0 - return just one value (see OPC HDA spec.)",  
+                                                                                v => ResampleInterval = Int32.Parse(v)},
+                { "raw",                    "Read raw data (if omitted, read processed data) ",  
+                                                                                v => ReadRaw = v != null},
+                { "m=|maxvalues=",          "Maximum number of values to load (should be checked at OPC server side, but doesn't work)", 
+                                                                                v => MaxValues = Int32.Parse(v)},
+                { "t=|tsformat=",           "Output timestamp format to use",   v => OutputTimestampFormat = v},
+                { "f=",                     "Output format (LIST or TABLE or MERGED)",   
+                                                                                v => OutputFormat = GetOutputFormat(v)},
+                { "o=|output=",             "Output filename (if omitted, output to console)",   
+                                                                                v => OutputFileName = v},
+                { "i=|input=",              "Input filename with list of tags (if omitted, tag list must be provided as command line argument)",   
+                                                                                v => InputFileName = v},
+                { "h|?|help",               "Show help",                        v => Help = v != null},
+                { "<>",                     "List of tag names",                v => Tagnames.Add (v)},
+            };
+
+            Console.Write("HDARead: ");
+            try {
+                p.Parse(args);
+            } catch (OptionException e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `HDARead --help' for more information.");
+                return false;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            TextWriter tw = new StringWriter(sb);
+            if(tw != null) {
+                p.WriteOptionDescriptions(tw);
+                tw.Flush();
+                OptionDescription = sb.ToString();
+            }
+            return true;
+        }
+
+
+        static bool CheckOptions() {
+            if (Help) {
+                ShowHelp();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Server)) {
+                Console.WriteLine("Missing required option s=|server=");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(InputFileName)) {
+                if (Tagnames.Count() < 1) {
+                    Console.WriteLine("No tagnames were specified.");
+                    return false;
+                }
+            } else {
+                if (Tagnames.Count() > 0) {
+                    Console.WriteLine("If the input file is specified, no tags may be entered as command line argument");
+                    return false;
+                }
+                // try catch !!!
+                Tagnames = File.ReadLines(InputFileName).ToList();
+                if (Tagnames.Count() < 1) {
+                    Console.WriteLine("No tagnames were specified.");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void ShowInfo() {
+            Console.WriteLine("HDARead is going to:");
+            Console.WriteLine("\t connect to OPC HDA server named '{0}' on computer '{1}'", Server, Host);
+            Console.WriteLine("\t and read {0} data for the period from '{1}' to '{2}'", ReadRaw ? "raw" : "processed", StartTime, EndTime);
+            Console.WriteLine("\t with resample interval {0} seconds", ResampleInterval, EndTime);
+            Console.WriteLine("\t for the following tags:");
+            foreach (string t in Tagnames) {
+                Console.WriteLine("\t\t" + t);
+            }
+            Console.WriteLine("\t No more than {0} values should be loaded (checked only at OPC server side).", MaxValues);
         }
 
         static void OutputList(StreamWriter sw, Opc.Hda.ItemValueCollection[] OPCHDAItemValues, string OutputTimestampFormat) {
@@ -200,6 +228,7 @@ namespace HDARead {
                 }
             }
         }
+
         static void OutputTable(StreamWriter sw, Opc.Hda.ItemValueCollection[] OPCHDAItemValues, string OutputTimestampFormat) {
             // header
             sw.Write("{0} timestamp, {0} value, {0} quality", OPCHDAItemValues[0].ItemName);
@@ -241,12 +270,13 @@ namespace HDARead {
                 sw.WriteLine();
             }
         }
-        static void ShowHelp(OptionSet p) {
+
+        static void ShowHelp() {
             Console.WriteLine("Usage: HDARead OPTIONS tag1 tag2 tag3");
             Console.WriteLine("HDARead is used to read the data from OPC HDA server.");
             Console.WriteLine();
             Console.WriteLine("Options:");
-            p.WriteOptionDescriptions(Console.Out);
+            Console.WriteLine(OptionDescription);
             Console.WriteLine();
             Console.WriteLine("Aggregates:");
             foreach (string agg in Enum.GetNames(typeof(HDAClient.OPCHDA_AGGREGATE))) {
